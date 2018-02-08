0b9b511a0c2ef359177bbe5e8752dbcdf872f33c
Valdemar26 <z_valdemar@ukr.net>
2018-02-08 17:46:21 +0200

    initial commit for a project Gif Battle on Angular 5

---
+++ src/app/core/header/header.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `
+    <nav class="navbar is-info">
+      <div class="container">
+            <!-- logo -->
+        <div class="navbar-brand">
+          <a class="navbar-item">Gif Battles</a>
+        </div>
+        <!-- menu -->
+        <div class="navbar-menu">
+          <div class="navbar-end">
+            <a class="navbar-item">Battle</a>
+            <a class="navbar-item">Create</a>
+            <a class="navbar-item">
+                <span class="glyphicon glyphicon-trash glyph"></span>
+                <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                Leaderboard
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  `,
+  styles: []
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
